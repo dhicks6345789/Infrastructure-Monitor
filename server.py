8c84bd7e8e998033f85610c79d13e5551637720f
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-10-09 22:24:51 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import socket
 
 
 
-# Parse a CSV / Excel file to read options.
+# Parse a CSV / Excel file to read options / settings configured by the user.
 def readOptionsFile(theFilename):
     state = 0
     options = [{}]
@@ -27,13 +27,11 @@ def readOptionsFile(theFilename):
                         if itemIndex != 0 and not pandas.isna(itemValue):
                             headerRow.append(itemValue)
                         else:
-                            #print(headerRow)
                             newDataFrame = pandas.DataFrame(columns=headerRow)
                             state = 3
                             rowIndex = 1
                     if state == 3:
                         if itemIndex == 0 and pandas.isna(itemValue):
-                            #print(newDataFrame)
                             options.append(newDataFrame)
                             newDataFrame = None
                             state = 0
@@ -66,52 +64,48 @@ print(school)
 iteration_counter = 0
 
 template_dir = os.path.abspath('./')
-app.debug=True
-
 htmlstr = ""
+cells = 10
 
-# Determine device - used to determine 
-user_agent = request.headers.get("User-Agent").lower()
-cells = 10 if "windows" in user_agent else 2
-print(f"{user_agent} + {cells}")
-
-def test_port(a,p):
+def test_port(a, p):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     result = s.connect_ex((a,p))
     return False if result else True   
 
-def build_table(dfList,c):
-    # c is the max number of cells per row. You want less on a phone.
+# c is the max number of cells per row. You want less on a phone.
+def build_table(dfList, c):
     def buildCell(name,result):
         col = "red" if result == False else "#C4D79B" 
         return f"<table><tr><td>{name}</td><td>&nbsp;</td></tr>\n<tr><td bgcolor='{col}'> </td><td>&nbsp;</td></tr></table>\n"
     newstr=""
-    # Create new table
+    
+    # Create new table.
     newstr += ("<table>\n<tr>\n")
-    cellcount=1
+    cellcount = 1
     for item in dfList:
-        newstr+=("<td><center>")
-        name=item[0]
-        res=item[1]
-        newstr+=buildCell(name,res)
-        if cellcount==c:
-            newstr+=("</tr><tr>")
-            cellcount=0
-        cellcount+=1   
-        newstr+=("<center></td>") 
-    newstr+=("</tr>\n</table>\n\n")
-    return newstr 
+        newstr += ("<td><center>")
+        name = item[0]
+        res = item[1]
+        newstr += buildCell(name,res)
+        if cellcount == c:
+            newstr += ("</tr><tr>")
+            cellcount = 0
+        cellcount += 1   
+        newstr += ("<center></td>") 
+    newstr += ("</tr>\n</table>\n\n")
+    return newstr
 
 for level in levels:      
-    # Title
-    htmlstr+=(f"<h2>{level[1]}</h2>\n")
-    device_results=[]
+    # Title.
+    htmlstr += (f"<h2>{level[1]}</h2>\n")
+    device_results = []
     filtered_devices = [tup for tup in devices if tup[3]==level[0]]
+    
     for device in filtered_devices:
         result = test_port(device[1],device[2])
         device_results.append([device[0],result])
 
-    htmlstr+=build_table(device_results,cells)
+    htmlstr += build_table(device_results,cells)
 iteration_counter += 1
-result = render_template('index.html', htmlstr=htmlstr, iter=iteration_counter, school=school)
+print(render_template('index.html', htmlstr=htmlstr, iter=iteration_counter, school=school))
