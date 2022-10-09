c14584c63b0667d897838af0f6d8fabacc1cdc00
David Hicks <48320403+dhicks6345789@users.noreply.github.com>
2022-10-09 22:54:34 +0100

    Update server.py

--- server.py
+++ server.py
@@ -70,7 +70,7 @@ cells = 10
 def test_port(a, p):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
-    result = s.connect_ex((a,p))
+    result = s.connect_ex((a, int(p)))
     return False if result else True   
 
 # c is the max number of cells per row. You want less on a phone.
